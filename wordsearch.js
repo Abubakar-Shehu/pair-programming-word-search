d395c6d2558308db0ebfee3e28f28c3155d66518
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-02 18:40:36 -0600

    Fixed the code to also search vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
 const wordSearch = (letters, word) => { 
+   
+    const vertical = transpose(letters);
+    
+    const verticalJoin = vertical.map(vls => vls.join(''))
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    for (const vl of verticalJoin) {
+        if (vl.includes(word)) return true
+    }
+
+    return false
 }
 
+// check vertical
+const transpose = function(matrix) {
+    // Replace this code with your solution
+    let returnmatrix = [];
+  
+    for (let i = 0; i < matrix[0].length; i++) {
+      let innerArray = [];
+      for (const col of matrix) {
+        innerArray.push(col[i]);
+      }
+      returnmatrix.push(innerArray);
+    }
+  
+    return returnmatrix;
+};
+
+// check if the word is found, and return false if not found
+
 module.exports = wordSearch
+
